122fc9f72d7c382244ae704e3a774dd08bd58c15
Renaud Durlin <renaud.durlin@gmail.com>
2021-07-01 00:03:16 +0200

    Generate tph and tra

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,64 @@
 const fs = require('fs');
 
-console.log(process.cwd());
+class DialogFile {
+  constructor(filename) {
+    this.content = fs.readFileSync(filename, 'utf8');
+    this.index = 0;
+  }
 
-const ref = fs.readFileSync('tra/dialogref.tra');
-const tra = fs.readFileSync('tra/dialog.tra');
+  getLine() {
+    if (this.index === this.content.length)
+      return undefined;
 
-console.log(ref.length);
-console.log(tra.length);
+    const begin = this.index++;
+    while (this.index < this.content.length) {
+      if (this.content[this.index] === '@' && this.content[this.index - 1] === '\n') {
+        break;
+      }
+      ++this.index;
+    }
 
-fs.writeFileSync('correctfrBG2EE/BG2EE.tph', 'foo');
-fs.writeFileSync('correctfrBG2EE/francais/correcfr.tra', 'bar');
+    const separator = this.content.indexOf('=', begin);
+    const nb = parseInt(this.content.substring(begin + 1, separator), 10);
+    const text = this.content.substring(separator + 1, this.index).trim();
+    return { nb, text };
+  }
+}
+
+function main() {
+  const ref = new DialogFile('tra/dialogref.tra');
+  const fix = new DialogFile('tra/dialog.tra');
+
+  const tphFD = fs.openSync('correctfrBG2EE/BG2EE.tph', 'ax');
+  const traFD = fs.openSync('correctfrBG2EE/francais/correcfr.tra', 'ax');
+
+  while (1) {
+    const refLine = ref.getLine();
+    const fixLine = fix.getLine();
+
+    if (refLine === undefined && fixLine === undefined)
+      break;
+
+    if (refLine === undefined)
+      throw { fixLine };
+
+    if (fixLine === undefined)
+      throw { refLine };
+
+    if (refLine.nb != fixLine.nb)
+      throw { refLine, fixLine };
+
+    if (refLine.text != fixLine.text) {
+      const offset = 1000000;
+      fs.appendFileSync(tphFD, '\tSTRING_SET ~' + fixLine.nb + '~ @' + (offset + fixLine.nb) + '\n', 'utf8');
+      fs.appendFileSync(traFD, '@' + (offset + fixLine.nb) + ' = ' + fixLine.text + '\n', 'utf8');
+    }
+  }
+}
+
+try {
+  main();
+}
+catch (e) {
+  console.error(e);
+}
